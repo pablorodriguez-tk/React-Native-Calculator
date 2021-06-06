38b31e153aa45d7acc123b4712e01846afc77b4d
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-06-06 17:58:12 -0300

    Initial config

---
+++ src/screens/CalculatorScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {styles} from '../theme/appTheme';
+
+export const CalculatorScreen = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Calculator Screen</Text>
+    </View>
+  );
+};
---
+++ src/theme/appTheme.tsx
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  backgroud: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  text: {
+    color: 'white',
+    fontSize: 20,
+  },
+});
